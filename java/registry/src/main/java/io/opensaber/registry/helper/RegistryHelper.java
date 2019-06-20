6b758b641272a892471e1e44cd0541bf2232d89b
Harikumar Palemkota <harip@ilimi.in>
2019-06-20 16:39:45 +0530

    Issue #SB-12974 created a separate layer which user-service calls and use registry functionality

---
+++ java/registry/src/main/java/io/opensaber/registry/helper/RegistryHelper.java
@@ -0,0 +1,84 @@
+package io.opensaber.registry.helper;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.opensaber.pojos.APIMessage;
+import io.opensaber.pojos.OpenSaberInstrumentation;
+import io.opensaber.pojos.Response;
+import io.opensaber.pojos.ResponseParams;
+import io.opensaber.registry.middleware.MiddlewareHaltException;
+import io.opensaber.registry.model.DBConnectionInfoMgr;
+import io.opensaber.registry.service.RegistryService;
+import io.opensaber.registry.sink.shard.Shard;
+import io.opensaber.registry.sink.shard.ShardManager;
+import io.opensaber.registry.util.RecordIdentifier;
+import io.opensaber.validators.IValidate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+public class RegistryHelper {
+
+    private static Logger logger = LoggerFactory.getLogger(RegistryHelper.class);
+
+    @Autowired
+    private APIMessage apiMessage;
+
+    @Autowired
+    private IValidate iValidate;
+
+    @Autowired
+    private ShardManager shardManager;
+
+    @Autowired
+    RegistryService registryService;
+
+    @Autowired
+    private DBConnectionInfoMgr dbConnectionInfoMgr;
+
+    @Autowired
+    private OpenSaberInstrumentation watch;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    public Response addEntity(JsonNode inputJson) throws MiddlewareHaltException {
+        ResponseParams responseParams = new ResponseParams();
+        Response response = new Response(Response.API_ID.CREATE, "OK", responseParams);
+        Map<String, Object> result = new HashMap<>();
+        String entityType = inputJson.fields().next().getKey();
+        String jsonString = objectMapper.convertValue(inputJson,String.class);
+        iValidate.validate(entityType, jsonString);
+
+        try {
+            Map requestMap = ((HashMap<String, Object>) apiMessage.getRequest().getRequestMap().get(entityType));
+            logger.info("Add api: entity type and shard propery: {}", shardManager.getShardProperty());
+            Object attribute = requestMap.getOrDefault(shardManager.getShardProperty(), null);
+            Shard shard = shardManager.getShard(attribute);
+
+            watch.start("RegistryController.addToExistingEntity");
+            String resultId = registryService.addEntity(jsonString);
+            RecordIdentifier recordId = new RecordIdentifier(shard.getShardLabel(), resultId);
+            Map resultMap = new HashMap();
+            String label = recordId.toString();
+            resultMap.put(dbConnectionInfoMgr.getUuidPropertyName(), label);
+
+            result.put(entityType, resultMap);
+            response.setResult(result);
+            responseParams.setStatus(Response.Status.SUCCESSFUL);
+            watch.stop("RegistryController.addToExistingEntity");
+            logger.info("AddEntity,{}", resultId);
+        } catch (Exception e) {
+            logger.error("Exception in controller while adding entity !", e);
+            response.setResult(result);
+            responseParams.setStatus(Response.Status.UNSUCCESSFUL);
+            responseParams.setErrmsg(e.getMessage());
+        }
+        return response;
+    }
+}
