001eb61699dd17c97799639da7983f966f14805d
Rajesh Rathnam <indrajra@users.noreply.github.com>
2019-07-10 16:35:37 +0530

    Issue #SC-1087 Fix readme (#10)
    * Issue #SC-1087 Fix readme
    
    * Issue #SC-1087 Merge and address review comments

--- sb-registry-configure-dependencies.sh
+++ sb-registry-configure-dependencies.sh
@@ -6,6 +6,7 @@ cp java/registry/src/main/resources/audit_frame.json.sample java/registry/src/ma
 rm java/registry/src/main/resources/public/_schemas/*
 cp sb_registry/schemas/* java/registry/src/main/resources/public/_schemas/
 cp sb_registry/application.yml.sample java/registry/src/main/resources/application.yml.sample
+cp java/registry/src/main/resources/application.yml java/registry/src/main/resources/application.yml.old
 cp java/registry/src/main/resources/application.yml.sample java/registry/src/main/resources/application.yml
 
 echo "Configuration of dependencies completed"
